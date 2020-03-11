7b2f422946c9f0c5fa109839fdad5b11a30a55c9
Logan Van Hook <loganvan@att.net>
2020-03-11 01:15:47 -0400

    Valid User ID

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const DB = require("./userDb");
 
 const router = express.Router();
 
@@ -17,9 +18,9 @@ router.get('/', logger, (req, res) =>
   
 });
 
-router.get('/:id', logger, (req, res) =>
+router.get('/:id', logger, validateUserId, (req, res) =>
 {
-  
+  console.log(req.user);
 });
 
 router.get('/:id/posts', logger, (req, res) =>
@@ -39,18 +40,46 @@ router.put('/:id', logger, (req, res) =>
 
 //custom middleware
 
+function failOut(res, err, msg)
+{
+  res.status(err).json({"error":msg});
+}
+
 function validateUserId(req, res, next)
+{
+  let ID = req.params.id;
+  if (!ID /* How are we even here?? */ || isNaN(ID))
+  {
+    failOut(res, 500, "Invalid request paramaters");
+    return;
+  }
+
+  DB.getById(ID).then((response)=>
+  {
+    if (response === undefined)
+    {
+      failOut(res, 404, "User not found");
+      return;
+    }
+    
+    req.user = response;
+    next();
+  }).catch((error)=>
+  {
+    failOut(res, 500, "Internal connection error");
+  });
+}
+
+function validateUser(req, res, next)
 {
   // do your magic!
 
 }
 
-function validateUser(req, res, next) {
+function validatePost(req, res, next)
+{
   // do your magic!
-}
 
-function validatePost(req, res, next) {
-  // do your magic!
 }
 
 function logger(req, res, next)
