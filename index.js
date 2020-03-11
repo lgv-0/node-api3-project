64cdc30dd06fa3cf34ba823d98cba573afa27dd0
Logan Van Hook <loganvan@att.net>
2020-03-11 00:52:01 -0400

    Initialize

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code away!
+const express = require("express");
+const server = express();
+server.use(express.json());
+
+const userRoute = require("./users/userRouter");
+server.use("/users", userRoute);
+
+server.listen(5000);
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,32 +2,41 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post('/', (req, res) =>
+{
+  logger(req.method, req.originalUrl, Date.now());
+
+  res.status(200).json({"msg":"ok"});
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/:id/posts', (req, res) =>
+{
+  logger(req.method, req.originalUrl, Date.now());
 });
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get('/', (req, res) =>
+{
+  logger(req.method, req.originalUrl, Date.now());
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', (req, res) =>
+{
+  logger(req.method, req.originalUrl, Date.now());
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get('/:id/posts', (req, res) =>
+{
+  logger(req.method, req.originalUrl, Date.now());
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', (req, res) =>
+{
+  logger(req.method, req.originalUrl, Date.now());
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', (req, res) =>
+{
+  logger(req.method, req.originalUrl, Date.now());
 });
 
 //custom middleware
@@ -42,6 +51,14 @@ function validateUser(req, res, next) {
 
 function validatePost(req, res, next) {
   // do your magic!
+}
+
+function logger()
+{
+  let Build = "";
+  for (var i = 0; i < arguments.length; i++)
+    Build += arguments[i] + (i !== arguments.length - 1 ? ", " : "");
+  console.log(Build);
 }
 
 module.exports = router;
